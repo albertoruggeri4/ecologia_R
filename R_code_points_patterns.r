c75e9a74d0684098f8a79ff4f0c2ba3998aac0ad
albertoruggeri4 <62298067+albertoruggeri4@users.noreply.github.com>
2020-03-31 15:04:58 +0200

    Update R_code_points_patterns.r

--- R_code_points_patterns.r
+++ R_code_points_patterns.r
@@ -22,3 +22,26 @@ plot(covid$country, covid$cases, las=3) #Etichette
 
 
 plot(covid$country, covid$cases, las=2, cex.axis= 0.7) #modifica dimensione carattere
+
+install.packages("ggplot2")
+install.packages("spatstat")
+
+library("ggplot2")
+library("spatstat")
+
+#mpg
+data("mpg")
+head("mpg")
+
+ggplot(mpg,aes(x=displ,y=hwy)) + geom_point()
+ggplot(covid,aes(x=lon,y=lat,size=cases)) + geom_point()
+
+#density
+#create dataset for spatstat, c per i vettori/range in questo caso della long e lat
+attach(covid) #in modo da allegare il nostro dataset con anche lon e lat
+covids <- ppp(lon, lat, c(-180,180), c(-90,90))
+
+d <- density(covids)
+plot(d) #andiamo a plottare la densità che abbiamo chiamato d
+
+points(covids) #se vuoi pch puoi aggiungerlo
